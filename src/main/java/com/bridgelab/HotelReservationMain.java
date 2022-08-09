7a727a5ae9e06b04410665e90f1d1a5154f0dbeb
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-10 00:27:02 +0530

    Added: Welcome To Hotel Reservation System

---
+++ src/main/java/com/bridgelab/HotelReservationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class HotelReservationMain {
+	public static void main(String[] args) {
+		System.out.println("----------Welcome To Hotel Reservation----------");
+	}
+
+}
